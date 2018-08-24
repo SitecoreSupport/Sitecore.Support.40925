a9f861b5ca1781907e0ea61966ff39f92ff16c18
DK\KIV <kirill.volovitskiy@sitecore.net>
2018-08-24 14:13:31 +0300

    default code

---
+++ src/Sitecore.Support.40925/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.40925")]
+[assembly: AssemblyProduct("Sitecore.Support.40925")]
+[assembly: ComVisible(false)]
